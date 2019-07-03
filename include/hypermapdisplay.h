f7675a8b5ff79452b341312a16793713f6aa9f6c
Tobias Zaenker <tobias.zaenker@aalto.fi>
2019-07-03 18:10:00 +0300

    HypermapDisplay can now display map from meta data

--- include/hypermapdisplay.h
+++ include/hypermapdisplay.h
@@ -3,31 +3,29 @@
 
 #include "rviz/display.h"
 #include "rviz/display_group.h"
-#include "rviz/default_plugin/image_display.h"
-#include "rviz/default_plugin/map_display.h"
-//#include "hypermap_msgs/HypermapImage.h"
+#include "rviz/properties/ros_topic_property.h"
+#include "rviz/properties/int_property.h"
+#include "rviz/properties/bool_property.h"
 #include "hypermap_msgs/HypermapMetaData.h"
 
 namespace hypermap
 {
 
-class HypermapDisplay : public rviz::Display
+class HypermapDisplay : public rviz::DisplayGroup
 {
 Q_OBJECT
 public:
   HypermapDisplay();
 
   // Overrides from Display
-  //virtual void onInitialize();
-  //virtual void fixedFrameChanged();
-  //virtual void reset();
   virtual void setTopic(const QString &topic, const QString &datatype);
+
+Q_SIGNALS:
+  void mapReceived();
 
 protected Q_SLOTS:
   void updateTopic();
   void updateLayerProps();
-  //void updateMap();
-  //* If this is true, will disable it's children when it's own bool value is false */
 
 protected:
   void receiveMapMeta(const hypermap_msgs::HypermapMetaData::ConstPtr& msg);
@@ -36,12 +34,11 @@ protected:
   ros::Subscriber map_sub_;
 
   rviz::RosTopicProperty *topic_property_;
-  rviz::IntProperty *layerCnt_;
-  rviz::BoolProperty **enableLayer_;
-  rviz::BoolProperty *enable_bg_property_;
 
 private:
-  int oldLayerCnt;
+  QString getDisplayName(const std::string &layerClass);
+  QString getTopicName(const std::string &layerClass, const std::string &layerName);
+  QString getTopicType(const std::string &layerClass);
 };
 
 }
--- src/hypermapdisplay.cpp
+++ src/hypermapdisplay.cpp
@@ -5,29 +5,23 @@
 #include <OgreImage.h>
 #include <OgreManualObject.h>
 
+#include "nav_msgs/OccupancyGrid.h"
+#include "hypermap_msgs/SemanticMap.h"
+
+#include "rviz/default_plugin/map_display.h"
+#include "semanticmapdisplay.h"
+
 PLUGINLIB_EXPORT_CLASS(hypermap::HypermapDisplay, rviz::Display)
 
 namespace hypermap
 {
 
-HypermapDisplay::HypermapDisplay() : rviz::Display()
+HypermapDisplay::HypermapDisplay() : rviz::DisplayGroup()
 {
     topic_property_ = new rviz::RosTopicProperty("Topic", "", ros::message_traits::datatype<hypermap_msgs::HypermapMetaData>(),
                                                  "hypermap_msgs::HypermapMetaData topic to subscribe to.", this, SLOT(updateTopic()));
-    layerCnt_ = new rviz::IntProperty("Layer count", 3, "Number of layers", this, SLOT(updateLayerProps()));
-    oldLayerCnt = 3;
-    layerCnt_->setMin(0);
-    layerCnt_->setMax(10);
-    enableLayer_ = new rviz::BoolProperty*[10];
-    for (int i = 0; i < 3; i++)
-    {
-        QString label = "Layer " + QString::number(i);
-        //std::cout << label.toStdString() << std::endl;
-        enableLayer_[i] = new rviz::BoolProperty(label, true, "", layerCnt_);
-    }
-    enable_bg_property_ = new rviz::BoolProperty("Enable background", true, "Display background of map", this);
-    //Display *disp = this->createDisplay("ImageDisplay");
-    //this->addDisplay(disp);
+
+    connect(this, SIGNAL(mapReceived()), this, SLOT(updateLayerProps()));
 }
 
 void HypermapDisplay::setTopic(const QString &topic, const QString &datatype)
@@ -41,51 +35,55 @@ void HypermapDisplay::updateTopic()
     map_sub_ = update_nh_.subscribe(topic_property_->getTopicStd(), 1, &HypermapDisplay::receiveMapMeta, this);
 }
 
+QString HypermapDisplay::getDisplayName(const std::string &layerClass)
+{
+    if (layerClass == "OccupancyGridLayer")
+        return "rviz/Map";
+    else if (layerClass == "SemanticLayer")
+        return "hypermap/SemanticMap";
+    else
+        return QString::fromStdString(layerClass) + QStringLiteral(" is not a valid layer class");
+}
+
+QString HypermapDisplay::getTopicName(const std::string &layerClass, const std::string &layerName)
+{
+    if (layerClass == "OccupancyGridLayer")
+        return QString::fromStdString(current_map_meta_.node_name) + QStringLiteral("/") + QString::fromStdString(layerName) + QStringLiteral("_map");
+    else if (layerClass == "SemanticLayer")
+        return QString::fromStdString(current_map_meta_.node_name) + QStringLiteral("/") + QString::fromStdString(layerName) + QStringLiteral("_semmap");
+    else
+        return QString::fromStdString(layerClass) + QStringLiteral(" is not a valid layer class");
+}
+
+QString HypermapDisplay::getTopicType(const std::string &layerClass)
+{
+    if (layerClass == "OccupancyGridLayer")
+        return ros::message_traits::datatype<nav_msgs::OccupancyGrid>();
+    else if (layerClass == "SemanticLayer")
+        return ros::message_traits::datatype<hypermap_msgs::SemanticMap>();
+    else
+        return QString::fromStdString(layerClass) + QStringLiteral(" is not a valid layer class");
+}
+
 void HypermapDisplay::updateLayerProps()
 {
-    /*if (layerCnt_->getInt() > oldLayerCnt)
+    removeAllDisplays();
+    for (const hypermap_msgs::LayerMetaData &layerMeta : current_map_meta_.layers)
     {
-        for (int i = oldLayerCnt; i < layerCnt_->getInt(); i++)
-        {
-            QString label = "Layer " + QString::number(i);
-            //std::cout << label.toStdString() << std::endl;
-            enableLayer_[i] = new rviz::BoolProperty(label, true, "", layerCnt_);
-        }
+        Display *disp = createDisplay(getDisplayName(layerMeta.class_name));
+        addDisplay(disp);
+        disp->setObjectName(QString::fromStdString(layerMeta.name));
+        disp->initialize(context_);
+        disp->setTopic(getTopicName(layerMeta.class_name, layerMeta.name), getTopicType(layerMeta.class_name));
+        disp->setEnabled(true);
     }
-    else if (layerCnt_->getInt() > oldLayerCnt)
-    {
-        for (int i = layerCnt_->getInt(); i < oldLayerCnt; i++)
-        {
-            //enableLayer_[i]->
-            //delete enableLayer_[i];
-        }
-    }
-    oldLayerCnt = layerCnt_->getInt();*/
 }
 
 void HypermapDisplay::receiveMapMeta(const hypermap_msgs::HypermapMetaData::ConstPtr& msg)
 {
     current_map_meta_ = *msg;
+    ROS_INFO("Map meta received");
+    Q_EMIT mapReceived();
 }
-
-/*void HypermapDisplay::onInitialize()
-{
-    Display::onInitialize();
-}
-
-void HypermapDisplay::fixedFrameChanged()
-{
-    Display::fixedFrameChanged();
-}
-
-void HypermapDisplay::reset()
-{
-    Display::reset();
-}
-
-void HypermapDisplay::updateMap()
-{
-
-}*/
 
 }
